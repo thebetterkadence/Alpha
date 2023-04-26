84e44300396ed15d971a945a9ccb8c67cf2077a9
π <kadencelamarque23@gmail.com>
2023-04-26 14:11:50 -0500

    i print link and first word

--- 01.py
+++ 01.py
@@ -1,4 +1,8 @@
 #print("Hello, world!")
-def helloWorld():
-    print("Hello, world!")
-helloWorld()
+#def helloWorld():
+#    print("Hello, world!")
+#helloWorld()
+#web scraping
+varWord = input()
+link = "https://www.congress.gov/search?q=%7B%22congress%22%3A%5B%22118%22%5D%2C%22source%22%3A%22all%22%2C%22search%22%3A%22" + varWord +"%20%22%7D"
+print(link)
