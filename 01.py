22f805cc3e9322351f55ea5e7d6a466ef2aba484
π <kadencelamarque23@gmail.com>
2023-04-27 14:45:31 -0500

    ooga booba im pi

--- 01.py
+++ 01.py
@@ -1,4 +1,10 @@
 from tkinter import *
+import tkinter.messagebox
+
+
+textBox = Entry(str(input("Search Bill")),Tk(), width=50,)
+textBox.pack()
+varWord = input()
 
 def create_window():
     #Tk() = new independent window
@@ -12,8 +18,9 @@ window.mainloop()
 #    print("Hello, world!")
 #helloWorld()
 #web scraping
+def firstLink():
+    varWord = input()
+    print(input("Enter keywords to access liable search results pertaining to any Congressional Bills."))
+    link = "https://www.congress.gov/search?q=%7B%22congress%22%3A%5B%22118%22%5D%2C%22source%22%3A%22all%22%2C%22search%22%3A%22" + varWord +"%20%22%7D"
+    print(link)
 
-varWord = input()
-print(input("Enter keywords to access liable search results pertaining to any Congressional Bills."))
-link = "https://www.congress.gov/search?q=%7B%22congress%22%3A%5B%22118%22%5D%2C%22source%22%3A%22all%22%2C%22search%22%3A%22" + varWord +"%20%22%7D"
-print(link)
